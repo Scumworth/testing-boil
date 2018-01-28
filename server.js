aa5be1e4802858a73d7dce231d72b360bc724f47
scumworth <danmcninch@gmail.com>
2018-01-28 16:28:10 -0500

    added setup.js with global vars

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.lisen(PORT, () => {
     console.log(`API running on port ${PORT}`);
--- test/sample.test.js
+++ test/sample.test.js
@@ -1,30 +1,32 @@
 // sample.test.js
 
-const { expect } = require('chai');
-const puppeteer = require('puppeteer');
-
-describe('Sample tests', () => {
-
-    // Globals
-    let browser;
+describe('sample test', () => {
     let page;
 
-    //Hooks
-    before(async () => {
-        browser = await puppeteer.launch();
-        page = await browser.newPage();
-    });
-    beforeEach(async () => {
+    before (async () => {
         page = await browser.newPage();
         await page.goto('http://localhost:3000');
     });
-    afterEach(async () => {
+
+    after (async () => {
         await page.close();
     });
-    after(async () => {
-        await browser.close();
+
+    it('should have correct title', async () => {
+        expect(await page.title()).to.eql('Puppeteer Testing Sample');
     });
-    
-    // Tests
-    
-});
+
+    it('should have a heading', async () => {
+        const HEADING_SELECTOR = 'h1';
+        let heading;
+        await page.waitFor(HEADING_SELECTOR);
+        heading = await page.$eval(HEADING_SELECTOR, heading => heading.innerText);
+        expect(heading).to.eql('Page Title);')
+    });
+
+    it('should have a single content section', async () => {
+        const BODY_SELECTOR = '.main-content';
+        await page.waitFor(BODY_SELECTOR);
+        expect(await page.$$(BODY_SELECTOR)).to.have.lengthOf(1);
+    })
+})
